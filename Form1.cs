c2b097128748f50f241165154e63ef814ba344ba
Recep Can <61096668+recepcantkn@users.noreply.github.com>
2022-03-15 08:33:31 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication27
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Form2 f2 = new Form2();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sizex;
+            int sizey;
+            sizex = int.Parse(textBox3.Text);
+            sizey = int.Parse(textBox4.Text);
+            f2.Size = new Size(sizex, sizey);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int top;
+            int left;
+            top = int.Parse(textBox1.Text);
+            left = int.Parse(textBox2.Text);
+            f2.Location = new Point(top, left);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            f2.Show();
+        }
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication27
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 f1 = new Form1();
+            this.Hide();
+            f1.Show();
+        }
+    }
+}
